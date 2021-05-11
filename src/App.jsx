2903c3c7c74e10ce8f5d989120148a13041bd108
Michal Puchmertl <michalp@xcroco.com>
2021-05-11 10:29:17 +0200

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import './styles.scss';
+
+function App() {
+  return (
+    <div className='container'>
+      <h1>Test</h1>
+    </div>
+  );
+}
+
+export default App;
